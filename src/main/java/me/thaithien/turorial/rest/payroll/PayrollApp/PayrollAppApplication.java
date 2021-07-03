8162cc7959bbfec286be2018471c9d27aecd214b
Thai Thien <tthien@apcs.vn>
2021-07-03 15:37:37 +0700

    yo, init

---
+++ src/main/java/me/thaithien/turorial/rest/payroll/PayrollApp/PayrollAppApplication.java
@@ -0,0 +1,13 @@
+package me.thaithien.turorial.rest.payroll.PayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/thaithien/turorial/rest/payroll/PayrollApp/PayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thaithien.turorial.rest.payroll.PayrollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
